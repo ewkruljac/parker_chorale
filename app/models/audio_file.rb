e9b5f40e50b115a57542fec1b992e6d198181fe8
Edward Kruljac <ewkruljac@gmail.com>
2016-11-03 12:18:11 -0600

    aws

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -1,5 +1,8 @@
 class AudioFile < ApplicationRecord
-  has_attached_file :mp3
+  has_attached_file :mp3,
+    :storage => :s3,
+    :s3_credentials => Proc.new{ |a| a.instance.s3_credentials }
+
   validates_attachment_content_type :mp3, :content_type => [
                                           'application/mp3',
                                           'application/x-mp3',
