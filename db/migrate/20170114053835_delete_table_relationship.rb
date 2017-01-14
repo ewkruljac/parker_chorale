1773fa74cdfa01ede3453c0d4a26f9c7ab4dd7b9
Edward Kruljac <ewkruljac@gmail.com>
2017-01-13 22:43:26 -0700

    Broke and reverted

---
+++ db/migrate/20170114053835_delete_table_relationship.rb
@@ -0,0 +1,5 @@
+class DeleteTableRelationship < ActiveRecord::Migration[5.0]
+  def change
+    drop_table :relationships
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111060122) do
+ActiveRecord::Schema.define(version: 20170114053835) do
 
   create_table "audio_files", force: :cascade do |t|
     t.string   "title"
