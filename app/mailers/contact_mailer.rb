abc4fb574429586108dce092ce629c0ac964b6f6
Edward Kruljac <ewkruljac@gmail.com>
2017-02-15 22:31:31 -0700

    Update mailers

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,8 @@
 class ContactMailer < ApplicationMailer
-  default from: 'parkerchorale@example.com'
+  default from: 'info@parkerchorale.org'
 
   def contact_email(contact)
     @contact = contact
-    mail(to: 'ewkruljac@gmail.com', subject: 'Thank You For Contacting Parker Chorale')
+    mail(to: 'info@parkerchorale.org', subject: 'Thank You For Contacting Parker Chorale')
   end
 end
