a1592010cce0e6f07027b046ddcd2293de984be3
Edward Kruljac <ewkruljac@gmail.com>
2017-05-06 19:25:28 -0600

    Home page updated to handle no current performances

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     @upcoming_performances = Performance.all
+    @home_page_performances = []
+    @upcoming_performances.each do |e|
+      @home_page_performances << e.title if e.end_date > Time.now
+    end 
   end
 
   def about
